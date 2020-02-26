7f54e0d585234035e166a43bd0a0cbcc8533e1d0
brandonabayer <53543478+brandonabayer@users.noreply.github.com>
2020-02-25 22:32:21 -0600

    [Starter/generateHTML.js], [Starter/index.js] HTML markup with object for user specific parameters

--- Starter/index.js
+++ Starter/index.js
@@ -1,11 +1,53 @@
+const axios = require("./git.ignore/node_modules/axios");
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateHTML = require("./generateHTML")
+const convertFactory = require('electron');
+// const open = require('open');
 const questions = [
-  
+    {
+        message: "Enter your GitHub username",
+        name: "username"
+    },
+    {
+        type: "list",
+        name: "color",
+        message: "What is your favorite color?",
+        choices: ["green", "blue", "pink", "red"]
+    }
 ];
 
-function writeToFile(fileName, data) {
- 
-}
+inquirer.prompt(questions).then(function ({ username, color }) {
+    const queryUrl = `https://api.github.com/users/${username}`;
+    // res response form axios
+    axios.get(queryUrl).then(function (res, err) {
+        if (err) console.log("error", err);
+        // console.log("string", res.data);
+        // console.log("string", res.owner);
+        return generateHTML({
+            color,
+            ...res.data
+        })
+        
 
-function init() {
+    }).then(html => {
+        var conversion = convertFactory({
+            converterPath: convertFactory.converters.PDF
+        });
+        conversion({ html }, function (err, result) {
+            if (err) {
+                return console.error("string", err);
+            }
+            console.log(result.numberOfPages);
+            console.log(result.logs);
+            result.stream.pipe(fs.createWriteStream(path.join(__dirname, "something.pdf")));
+            conversion.kill(); // necessary if you use the electron-server strategy, see bellow for details
+        });
+     })
+});
+
+function writeToFile(fileName, data) { }
+
+function init() { }
 
 init();
